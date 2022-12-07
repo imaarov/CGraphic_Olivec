28e55c3b9e3d609dd74e939054f939a5d52e5745
imaaarov <imanstanxz@gmail.com>
2022-12-07 16:01:22 +0330

    add checkered board shape

--- src/example.c
+++ src/example.c
@@ -6,13 +6,13 @@
 #include "olive.c"
 
 #define WIDTH 800
-#define HEIGHT 800
+#define HEIGHT 600
 
 static uint32_t pixels[HEIGHT * WIDTH];
 
 void olive_fill_rect(uint32_t *pixels, size_t pixels_width,
-                     size_t pixels_height, int x0, int y0, size_t w,
-                     size_t h ,uint32_t color) {
+                     size_t pixels_height, int x0, int y0, size_t w, size_t h,
+                     uint32_t color) {
   for (int dy = 0; dy < (int)h; ++dy) {
     int y = y0 + dy;
     if (0 <= y && y < (int)pixels_height) {
@@ -26,11 +26,28 @@ void olive_fill_rect(uint32_t *pixels, size_t pixels_width,
   }
 }
 
+#define COLS 8
+#define ROWS 6
+#define CELL_WIDTH (WIDTH / COLS)
+#define CELL_HEIGHT (HEIGHT / ROWS)
+
 int main(void) {
   olivec_fill(pixels, WIDTH, HEIGHT, 0xFF202020);
-  size_t rw = 50 * 4;
-  size_t rh = 30 * 4;
-  olive_fill_rect(pixels, WIDTH, HEIGHT, WIDTH / 2 - rw / 2, HEIGHT / 2 - rh / 2, rw, rh, 0xFF2020FF);
+
+  for (int y = 0; y < ROWS; ++y) {
+    for (int x = 0; x < COLS; ++x) {
+      uint32_t color;
+      if ((x + y) % 2 == 0) {
+        color = 0xFF0000FF;
+      } else {
+        color = 0xFF00FF00;
+      }
+      olive_fill_rect(pixels, WIDTH, HEIGHT, x * CELL_WIDTH, y * CELL_HEIGHT,
+                      CELL_WIDTH, CELL_HEIGHT, color);
+    }
+  }
+  // size_t rw = 50 * 4;
+  // size_t rh = 30 * 4;
   const char *file_path = "output.ppm";
   Errno err = olive_save_to_ppm_file(pixels, WIDTH, HEIGHT, file_path);
   if (err) {
